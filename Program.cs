1fe2e86ef6cf38e03ec0d9daecb4ee8b9bbdd734
MegaPapa <maks1464@gmail.com>
2016-11-20 20:43:49 +0300

    Text analyzer v.0.1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPP_TextAnalyzer.TextProcessor;
+using System.Linq.Expressions;
+
+namespace MPP_TextAnalyzer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            ExpressionTreeCreator.ExpressionTreeCreator tree = new ExpressionTreeCreator.ExpressionTreeCreator("Maxim@AND@(@Minsk@OR@Molodechno@)@OR@(@Kek@AND@Lol@)");
+            
+            String[] text = { "Kek","Lol","Yoba" };
+            Func<String[],Boolean> myLambda = tree.CreateLambda();
+            
+            Console.WriteLine(myLambda(text));
+            String[] words = { "Maxim","Minskf","NOMAZ","LELKEKES","ZZZZZ","SASAI","U","LUKOMORIA","DUB","ZELENII" };
+            Console.WriteLine(myLambda(words));
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ TextProcessor/TextProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_TextAnalyzer.TextProcessor
+{
+    public class TextProcessor
+    {
+
+        private static String[] words;
+        public static String[] Words
+        {
+            get { return words; }
+            set { words = value; }
+        }
+        public static Boolean FindInText(String word,String[] words)
+        {
+            if (Array.IndexOf(words,word) != -1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
