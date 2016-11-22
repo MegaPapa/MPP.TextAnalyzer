0c83d4b7dfc8ea517f2a78b26d9f01302da1994b
MegaPapa <maks1464@gmail.com>
2016-11-23 00:41:47 +0300

    text processor fix

--- MPP.TextAnalyzer/TextProcessor/TextProcessor.cs
+++ MPP.TextAnalyzer/TextProcessor/TextProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -10,6 +11,25 @@ namespace MPP_TextAnalyzer.TextProcessor
 {
     public static class TextProcessor
     {
+
+        public static String[] GetStringArrayFromFile(String path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("File not found.");
+            Encoding rusEncoding = Encoding.GetEncoding(1251);
+            StringBuilder text = new StringBuilder();
+            String tmp;
+            StreamReader streamReader = new StreamReader(@path, rusEncoding);
+            while ((tmp = (streamReader.ReadLine())) != null)
+                text.Append(tmp);
+            return SplitTextOnWords(text.ToString());
+        }
+
+        private static String[] SplitTextOnWords(String text)
+        {
+            Char[] punctuationMarks = { ' ', ',', ':', '?', '!',';' };
+            return text.Split(punctuationMarks, StringSplitOptions.RemoveEmptyEntries);
+        }
 
         public static Boolean FindInText(String word,String[] words)
         {
